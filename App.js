f79d759b1ae897b78f04c55c38024c009da316de
Daniel <eoyeniran@tradedept.co>
2019-05-09 08:18:57 +0100

    Semicolons add

--- App.js
+++ App.js
@@ -15,8 +15,8 @@ export default class App extends Component {
   }
 
   handleChange = (state) => {
-    console.log({ state })
-    this.setState({ state })
+    console.log({ state });
+    this.setState({ state });
   }
 
   render() {
